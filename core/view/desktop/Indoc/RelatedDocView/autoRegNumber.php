41b7724d928fc7f621e6d6a5d5fcb7b5a0a37b79
TheSermux <70709608+TheSermux@users.noreply.github.com>
2021-10-29 00:07:05 +0300

    added function of autmated creating registration number + modify redacting of timestamp

--- core/view/desktop/Indoc/RelatedDocView/autoRegNumber.php
+++ core/view/desktop/Indoc/RelatedDocView/autoRegNumber.php
@@ -10,28 +10,26 @@ use RedCore\Infodocs\Collection as Infodocs;
 
 $where = $where = Where::Cond()
     ->add("_deleted", "=", "0")
-    ->add("and")
-    ->add("doc_id", "=", $doc_id)
     ->parse();
 
-if (0 != $doc_id) {
-    Indoc::setObject("orelateddocs");
-    $relateddocs = Indoc::getList($where);
-}
 
+// get table oindoc
 Indoc::setObject("oindoc");
 $docs = Indoc::getList();
 
-
+// get registration numbers from table
 $a = array();
     foreach($docs as $item) {
-		array_push($a, <?= $item->object->reg_number ?>)
+		$c = $item->object->reg_number;
+		array_push($a, $c);
 	}
+
+//searching max value and increment it
 $max = max($a);
 $new_number = $max +1;
-//$stringNewNum = (string) $new_number;
+$stringNewNum = (string) $new_number;
 
 
-
-return $new_number;
+return $stringNewNum;
+//return $new_number;
 ?>
--- core/view/desktop/Indoc/formindoc.addupdate.php
+++ core/view/desktop/Indoc/formindoc.addupdate.php
@@ -59,20 +59,18 @@ if (is_null(Request::vars("oindoc_id"))) {
     $step_role = $user_role;
 }
 
-//  disabling reg_data 
-$dis_reg_data = "disabled";
+//  disabling form by role
+$disable_form = "disabled";
 
 if( $user_role == "2" ) {
-	$dis_reg_data = "";
+	$disable_form = "";
 }
 if( $user_role == "1" ) {
-	$dis_reg_data = "";
+	$disable_form = "";
 }
 if( $user_role == "19" ) {
-	$dis_reg_data = "";
+	$disable_form = "";
 }
-
-
 
 
 if (is_null($doc_type)) {
@@ -82,6 +80,12 @@ Session::set("s_relateddoc_id", $oindoc_item->object->id);
 $relateddocs = require('RelatedDocView/generateRelatedDocView.php');
 Session::delete("s_relateddoc_id", $oindoc_item->object->id);
 
+$auto_reg_number = require('RelatedDocView/autoRegNumber.php');
+$reg_number = $oindoc_item->object->reg_number;
+if (is_null($reg_number)) {
+
+	$reg_number = $auto_reg_number;
+}
 
 $form = Forms::Create()
     ->add("action", "action", "hidden", "action", $html_object . ".store.do", 6, false)
@@ -92,8 +96,8 @@ $form = Forms::Create()
     ->add("step_role", "step_role", "hidden", $html_object . "[step_role]", $step_role)
     ->add("doctypes", "Тип документа", "select", $html_object . "[params][doctypes]", $doc_type, 6, false, $DocTypesResult)
     ->add("name_doc", "Имя документа", "text", $html_object . "[name_doc]", $oindoc_item->object->name_doc)
-    ->add("reg_number", "№ Регистрации", "text", $html_object . "[reg_number]", $oindoc_item->object->reg_number)
-    ->add("reg_date", "Дата регистрации", "text", $html_object . "[reg_date]", $oindoc_item->object->reg_date, "", "", "", $dis_reg_data)
+    ->add("reg_number", "№ Регистрации", "text", $html_object . "[reg_number]", $reg_number, "", "", "", $disable_form)
+    ->add("reg_date", "Дата регистрации", "text", $html_object . "[reg_date]", $oindoc_item->object->reg_date, "", "", "", $disable_form)
 
     ->add("html", "", "html", "", '<src="' . CMS_TMP . SEP . $oindoc_item->object->params->file_title . '">')
     ->add("file", "Файл", "file", $html_object . "[file]")
